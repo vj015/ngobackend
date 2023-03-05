9087954d93cbe4ef619697e7136f4b7a681d6473
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-03-06 01:59:44 +0530

    First commit to test connectivity

---
+++ ngobackend/src/main/java/com/ngobackend/NgobackendApplication.java
@@ -0,0 +1,13 @@
+package com.ngobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgobackendApplication.class, args);
+	}
+
+}
---
+++ ngobackend/src/test/java/com/ngobackend/NgobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
