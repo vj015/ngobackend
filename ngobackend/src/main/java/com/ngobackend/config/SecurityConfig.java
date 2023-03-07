a93c62452b112c7ecf88cc8345b121bb0ab568a3
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-03-08 01:54:49 +0530

    Enable Web Security from form based security

---
+++ ngobackend/src/main/java/com/ngobackend/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.ngobackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+          
+        return http.build();
+    }
+
+}
