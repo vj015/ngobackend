a2d7852844c2d474003636fdccabdcaeac7472e1
Vipul Jain <75699731+vj015@users.noreply.github.com>
2023-03-30 23:18:59 +0530

    Updated

--- ngobackend/src/main/java/com/ngobackend/controller/AuthController.java
+++ ngobackend/src/main/java/com/ngobackend/controller/AuthController.java
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ngobackend.dto.JwtAuthRequest;
 import com.ngobackend.dto.JwtAuthResponse;
+import com.ngobackend.dto.Loggedinuser;
 import com.ngobackend.security.CustomUserDetailService;
 import com.ngobackend.security.jwtUtil;
 import com.ngobackend.services.Userservice;
@@ -37,12 +40,21 @@ public class AuthController {
 	@PostMapping("/login")
 	public ResponseEntity<JwtAuthResponse> createToken(@RequestBody JwtAuthRequest jwtAuthRequest) throws Exception
 	{
-		this.authenticate(jwtAuthRequest.getUsername(), jwtAuthRequest.getPassword());
-		UserDetails userDetails = this.customUserDetailService.loadUserByUsername(jwtAuthRequest.getUsername());
-		String token = this.JwtUtil.generateToken(userDetails);
-		JwtAuthResponse res = new JwtAuthResponse();
-		res.setToken(token);
-		return new ResponseEntity<JwtAuthResponse>(res, HttpStatus.OK);
+		try {
+			this.authenticate(jwtAuthRequest.getUsername(), jwtAuthRequest.getPassword());
+			UserDetails userDetails = this.customUserDetailService.loadUserByUsername(jwtAuthRequest.getUsername());
+			String token = this.JwtUtil.generateToken(userDetails);
+			JwtAuthResponse res = new JwtAuthResponse();
+			res.setToken(token);
+			return new ResponseEntity<JwtAuthResponse>(res, HttpStatus.OK);
+			
+		} catch (BadCredentialsException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} catch(Exception e)
+		{
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 	
 	private void authenticate(String username, String password) throws Exception
@@ -54,7 +66,7 @@ public class AuthController {
 		} catch (BadCredentialsException e) {
 			// TODO: handle exception
 			e.printStackTrace();
-			throw new Exception("Invalid username or password");
+			throw new BadCredentialsException("Invalid username or password");
 		} catch(Exception e){
 			e.printStackTrace();
 			throw new Exception("Failed error");			
